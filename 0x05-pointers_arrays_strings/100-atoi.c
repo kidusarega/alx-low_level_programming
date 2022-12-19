8fd93b8a29f4646ed92c4aca15cb32d2f21c9d43
Kidus <105130559+kidusarega@users.noreply.github.com>
2022-12-19 21:41:14 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,36 +1,40 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * _atoi - function converts string to an integer
- * @s: Pointer parameter
- * Return: returns an integer value
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
+ *
+ * Return: A integer
  */
 int _atoi(char *s)
 {
-	unsigned int count = 0, size = 0, j = 0, k = 1, m = 1, i;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	while (*(s + count) != '\0')
+	while (s[c])
 	{
-		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-			break;
-
-		if (*(s + count) == '-')
-			k *= -1;
-
-		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		if (s[c] == 45)
 		{
-			if (size > 0)
-				m *= 10;
-			size++;
+			min *= -1;
 		}
-		count++;
+
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+
+		if (isi == 1)
+		{
+			break;
+		}
+
+		c++;
 	}
 
-	for (i = count - size; i < count; i++)
-	{
-		j = j + ((*(s + i) - 48) * m);
-		m /= 10;
-	}
-	return (j * k);
+	ni *= min;
+	return (ni);
 }
